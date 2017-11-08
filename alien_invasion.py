2b90c48464adee122ff00583d861f64a90626a8b
Michael <michael0304@126.com>
2017-11-08 23:49:23 +0800

    add script for 12.3.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,25 @@
+# -*- coding:utf-8 -*-
+
 import sys
 import pygame
 
 def run_game():
+	#初始化游戏并创建一个屏幕对象
     pygame.init()
     screen = pygame.display.set_mode((1200,600))  # 800 is too big for my screen
     pygame.display.set_caption("Alien Invasion")
+ 
+    bg_color = (230, 230, 230) #set background color:
 
+    # 开始游戏的主循环
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        pygame.display.flip()
+        screen.fill(bg_color)  # 每次循环都重绘屏幕
 
-
+        pygame.display.flip() # 让最近绘制的屏幕可见
 
 run_game()
 
