32d26b8a761db257f4e2a30fc0c9f555ea081cdb
Michael <michael0304@126.com>
2017-11-09 23:06:34 +0800

    modify script for 12.3.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,25 +1,26 @@
 # -*- coding:utf-8 -*-
 
-import sys
+import sys  #为退出游戏使用
 import pygame
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
-    pygame.init()
-    screen = pygame.display.set_mode((1200,600))  # 800 is too big for my screen
+    pygame.init()  #初始化背景设置
+    screen = pygame.display.set_mode((1200,600))  # 创建一个名为screen的显示窗口，元组指定游戏窗口的大小
     pygame.display.set_caption("Alien Invasion")
  
     bg_color = (230, 230, 230) #set background color:
 
     # 开始游戏的主循环
     while True:
+    	#监视键盘和鼠标事件
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT: #表示单击游戏窗口的关闭按钮
                 sys.exit()
 
-        screen.fill(bg_color)  # 每次循环都重绘屏幕
+        screen.fill(bg_color)  # 用背景色填充屏幕，每次循环都重绘屏幕
 
-        pygame.display.flip() # 让最近绘制的屏幕可见
+        pygame.display.flip() # 让最近绘制的屏幕可见，并擦去旧屏幕
 
 run_game()
 
