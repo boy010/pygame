7413551c9812870e4730e6d131ed32998e46b8e2
Michael <michael0304@126.com>
2017-11-14 23:05:08 +0800

    modify the error in ship.py

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship():
 
 		#加载飞船图像并获取其外接矩形
 		self.image = pygame.image.load('images/ship.bmp')
-		self.rect = self.imag.get_rect()
+		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
 
 		#将每艘飞船放在屏幕底部
