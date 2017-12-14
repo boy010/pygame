2ddada57edf294281fb2d5c9a090915feabce02c
FengJian <michael0304@126.com>
2017-12-14 20:39:10 +0800

    add script for 13.6.1 检测外星人和飞船的碰撞

--- game_functions.py
+++ game_functions.py
@@ -124,7 +124,11 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def update_aliens(ai_settings, aliens):
+def update_aliens(ai_settings, ship, aliens):
     '''检查是否有外星人位于屏幕边缘，并更新外星人群中所有外星人的位置'''
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
+
+    #检测外星人和飞船的碰撞
+    if pygame.sprite.spritecollideany(ship, aliens):
+    	print("Ship hit!!!")
